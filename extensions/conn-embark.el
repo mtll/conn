1219d54aa474767dc1cb1ff85be269b85adec804
David <dave.feller@gmail.com>
2024-03-28 15:58:48 -0600

    make xref-find-references default alt key for identifiers

--- extensions/conn-embark.el
+++ extensions/conn-embark.el
@@ -271,7 +271,7 @@ will navigate up out of a keymap."
          conn-embark-alt-expression-map (define-keymap conn-embark-alt-key 'comment-region)
          conn-embark-alt-defun-map      (define-keymap conn-embark-alt-key 'comment-defun)
          conn-embark-alt-heading-map    (define-keymap conn-embark-alt-key 'narrow-to-heading)
-         conn-embark-alt-identifier-map (define-keymap conn-embark-alt-key 'describe-symbol))
+         conn-embark-alt-identifier-map (define-keymap conn-embark-alt-key 'xref-find-references))
         (pcase-dolist (`(,map . ,thing) '((conn-embark-alt-expression-map . expression)
                                           (conn-embark-alt-symbol-map     . symbol)
                                           (conn-embark-alt-defun-map      . defun)
