44b519ad7b51039b5bb597dbc15a9d8a6ffec27b
David <dave.feller@gmail.com>
2024-06-17 08:45:02 -0600

    fix thing definitions

--- extensions/conn-evil-treesit-obj.el
+++ extensions/conn-evil-treesit-obj.el
@@ -45,54 +45,61 @@
                          bash-ts-mode
                          typescript-ts-mode))
 
-(conn-register-thing inner-function
-  :modes conn--ts-modes
-  :mark-key "f"
-  :bounds-op (lambda ()
-               (evil-textobj-tree-sitter--thing-at-point-bounds "function.inner")))
+(conn-register-thing
+ 'inner-function
+ :modes conn--ts-modes
+ :mark-key "f"
+ :bounds-op (lambda ()
+              (evil-textobj-tree-sitter--thing-at-point-bounds "function.inner")))
 
-(conn-register-thing inner-loop
-  :modes conn--ts-modes
-  :mark-key "o"
-  :bounds-op (lambda ()
-               (evil-textobj-tree-sitter--thing-at-point-bounds "loop.inner")))
+(conn-register-thing
+ 'inner-loop
+ :modes conn--ts-modes
+ :mark-key "o"
+ :bounds-op (lambda ()
+              (evil-textobj-tree-sitter--thing-at-point-bounds "loop.inner")))
 
-(conn-register-thing inner-conditional
-  :modes conn--ts-modes
-  :mark-key "i"
-  :bounds-op (lambda ()
-               (evil-textobj-tree-sitter--thing-at-point-bounds "conditional.inner")))
+(conn-register-thing
+ 'inner-conditional
+ :modes conn--ts-modes
+ :mark-key "i"
+ :bounds-op (lambda ()
+              (evil-textobj-tree-sitter--thing-at-point-bounds "conditional.inner")))
 
-(conn-register-thing inner-assignment
-  :modes conn--ts-modes
-  :mark-key "a"
-  :bounds-op (lambda ()
-               (evil-textobj-tree-sitter--thing-at-point-bounds "assignment.inner")))
+(conn-register-thing
+ 'inner-assignment
+ :modes conn--ts-modes
+ :mark-key "a"
+ :bounds-op (lambda ()
+              (evil-textobj-tree-sitter--thing-at-point-bounds "assignment.inner")))
 
-(conn-register-thing inner-class
-  :modes conn--ts-modes
-  :mark-key "c"
-  :bounds-op (lambda ()
-               (evil-textobj-tree-sitter--thing-at-point-bounds "class.inner")))
+(conn-register-thing
+ 'inner-class
+ :modes conn--ts-modes
+ :mark-key "c"
+ :bounds-op (lambda ()
+              (evil-textobj-tree-sitter--thing-at-point-bounds "class.inner")))
 
-(conn-register-thing inner-comment
-  :modes conn--ts-modes
-  :mark-key ";"
-  :bounds-op (lambda ()
-               (evil-textobj-tree-sitter--thing-at-point-bounds "comment.inner")))
+(conn-register-thing
+ 'inner-comment
+ :modes conn--ts-modes
+ :mark-key ";"
+ :bounds-op (lambda ()
+              (evil-textobj-tree-sitter--thing-at-point-bounds "comment.inner")))
 
-(conn-register-thing inner-parameter
-  :modes conn--ts-modes
-  :mark-key "p"
-  :bounds-op (lambda ()
-               (evil-textobj-tree-sitter--thing-at-point-bounds "parameter.inner")))
+(conn-register-thing
+ 'inner-parameter
+ :modes conn--ts-modes
+ :mark-key "p"
+ :bounds-op (lambda ()
+              (evil-textobj-tree-sitter--thing-at-point-bounds "parameter.inner")))
 
-(conn-register-thing function    :modes conn--ts-modes :mark-key "F")
-(conn-register-thing loop        :modes conn--ts-modes :mark-key "O")
-(conn-register-thing conditional :modes conn--ts-modes :mark-key "I")
-(conn-register-thing assignment  :modes conn--ts-modes :mark-key "A")
-(conn-register-thing class       :modes conn--ts-modes :mark-key "C")
-(conn-register-thing comment     :modes conn--ts-modes :mark-key ":")
-(conn-register-thing parameter   :modes conn--ts-modes :mark-key "P")
+(conn-register-thing 'function    :modes conn--ts-modes :mark-key "F")
+(conn-register-thing 'loop        :modes conn--ts-modes :mark-key "O")
+(conn-register-thing 'conditional :modes conn--ts-modes :mark-key "I")
+(conn-register-thing 'assignment  :modes conn--ts-modes :mark-key "A")
+(conn-register-thing 'class       :modes conn--ts-modes :mark-key "C")
+(conn-register-thing 'comment     :modes conn--ts-modes :mark-key ":")
+(conn-register-thing 'parameter   :modes conn--ts-modes :mark-key "P")
 
 (provide 'conn-evil-treesit-obj)
