b91c1c894a12c7f45b2b356d29aa58d0677f136f
david feller <dave.feller@gmail.com>
2025-07-28 09:00:47 -0600

    fix surround-with org handler

--- conn-org.el
+++ conn-org.el
@@ -284,6 +284,20 @@
   "E" 'org-export
   "e" 'org-example)
 
+(cl-defmethod conn-handle-surround-with-argument :extra "Org" (cmd arg)
+  (if (memq cmd '(org-quote
+                  org-center
+                  org-comment
+                  org-src
+                  org-verse
+                  org-export-ascii
+                  org-export-html
+                  org-export-latex
+                  org-export
+                  org-example))
+      (conn-set-argument-value arg (list cmd (conn-state-loop-consume-prefix-arg)))
+    (cl-call-next-method)))
+
 (cl-defmethod conn-perform-surround ((_cmd (eql org-quote)) _arg
                                      &key &allow-other-keys)
   (org-insert-structure-template "quote"))
