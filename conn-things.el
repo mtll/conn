fea07986508d75714ef3519671c1a44bbd66c3be
david feller <dave.feller@gmail.com>
2025-10-28 16:24:40 -0600

    more anonymous thing

--- conn-things.el
+++ conn-things.el
@@ -143,6 +143,7 @@
   "Make an anonymous thing."
   (declare (indent 0))
   (cl-assert (plistp properties))
+  (require 'cl-lib)
   (cl-loop with known = (get 'conn-anonymous-thing :known-properties)
            with seen = nil
            for (key var) on properties by #'cddr
@@ -158,9 +159,10 @@
                                    (cnm (gensym "thing--cnm")))
                                `#'(lambda ,(cons cnm args)
                                     ,@(car parsed-body)
-                                    ,(macroexpand-all
-                                      `(cl-flet ((cl-call-next-method ,cnm))
-                                         ,@(cdr parsed-body))))))
+                                    ,@(macroexp-unprogn
+                                       (macroexpand-all
+                                        `(cl-flet ((cl-call-next-method ,cnm))
+                                           ,@(cdr parsed-body)))))))
                             (result (error "Unexpected macroexpansion result :%S"
                                            result))))))
                    `(',(car seen)
